c5cc9c220a95c6f646a28cad4a9899d0b4d8fc9a
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-04 15:02:51 +0700

    Setup MongoDB dengan mongoose

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const connectDB = require("./config/db");
+connectDB();
+
 const app = express();
 
 // Middleware
