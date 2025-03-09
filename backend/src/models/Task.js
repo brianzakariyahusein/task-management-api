3c5f7b0bc1bf339981c0dcdfec580358827541ab
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-09 15:28:27 +0700

    membuat model task (database schema)

---
+++ backend/src/models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
